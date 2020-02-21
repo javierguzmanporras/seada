671fcbec15971ee420b52db5dae4df102b0aa228
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-02-21 06:44:59 +0100

    add new functions, and tqdm

--- seada/TwitterFriend.py
+++ seada/TwitterFriend.py
@@ -1,30 +1,40 @@
-import tweepy
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import tweepy
+import sys
+from tqdm import tqdm
 
 class Friends():
     """Information about a Twitter user's friends"""
 
     def __init__(self):
-        pass
+        #self.user_id = ""
+        self.user_friends = []
 
     def get_user_friends(self,api,username):
 
         nfriends = 0
 
-        for userid in tweepy.Cursor(api.friends_ids, screen_name=username).items():
-            user = api.get_user(userid)
-            print(user.screen_name)
-            nfriends += 1
+        user = api.get_user("@jgp_ingTeleco")
+        my_total = user.friends_count
+
+        for userid in tqdm(tweepy.Cursor(api.friends_ids, screen_name=username).items(), total=my_total,
+                           desc="Get Friends"):
+            try:
+                user = api.get_user(userid)
+                self.user_friends.append(user)
+                #print(str(nfriends) + " friend name: " + user.screen_name)
+                nfriends += 1
+            except:
+                print("error" + str(sys.exc_info()))
+
 
         print("Num friends: " + str(nfriends))
+        if "arduino" in self.user_friends:
+            print("arduino ok")
 
+        if "algoquenoesta" in self.user_friends:
+            print("algoquenoesta ko")
 
-
-
-
-    # code for print all friends
-    # friends = api.friends_ids()
-    # friends = api.friends(count=50)
-    # print(len(friends))
-    # for id in friends:
-    #    print(id)
+            #https://www.rittmanmead.com/blog/2015/08/three-easy-ways-to-stream-twitter-data-into-elasticsearch/
