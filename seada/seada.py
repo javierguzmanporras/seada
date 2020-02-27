c51ae8a652285d0d21e5cd161bf21a5134119a6f
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-02-27 06:36:27 +0100

    Changes: 1) New import and reorganize old imports 2) Add empty documentation for all functions 3) add exceptions in banner function 4) change positions of arguments 5) complete config function

--- seada/seada.py
+++ seada/seada.py
@@ -3,52 +3,71 @@
 # DEASOS - Data Extraction and Analysis System from Open Sources
 
 import argparse
-import platform
-import os, sys
+import os
+import sys
+import tweepy
 
 __version__ = 0.1
 
 
 def banner():
-    f = open("banner2.txt",'r')
-    banner = f.readlines()
-    for line in banner:
-        print(line.replace("\n",""))
-    f.close()
+    """
+    Function
+    :return:
+    """
+    try:
+        banner_file = open("banner2.txt", 'r')
+        for line in banner_file.readlines():
+            print(line.replace("\n", ""))
+        banner_file.close()
+    except FileNotFoundError:
+        print('[seada.banner] Error: The banner file not found')
+    except Exception as exception:
+        print(exception)
+        raise
 
 
 def parse_args():
+    """
+
+    :return:
+    """
     parser = argparse.ArgumentParser(prog='seada.py',
                                      description='Sistema de Extracción y Análisis de Datos de fuentes Abiertas',
                                      epilog='Enjoy the program! :)')
-    parser.add_argument('--account', '-a', metavar='ACCOUNT', type=str, help='User twitter account')
-    parser.add_argument('--account-list', '-al', metavar='ACCOUNT-LIST', type=str, help='User list twitter account')
-    parser.add_argument('--output', '-o', choices=['csv', 'json'], help='Type of file output')
-    parser.add_argument('--version', '-v', action='version', version=f"%(prog)s {__version__}")
-    parser.add_argument('--test', '-t', help='test')
+    parser.add_argument('-a', '--account', metavar='ACCOUNT', type=str, help='User twitter account')
+    parser.add_argument('-al', '--account-list', metavar='ACCOUNT-LIST', type=str, help='User list twitter account')
+    parser.add_argument('-o', '--output', choices=['csv', 'json'], help='Type of file output')
+    parser.add_argument('-v', '--version', action='version', version=f"%(prog)s {__version__}")
     args = parser.parse_args()
     return args
 
 
-def config():
+def config_twitter_api():
+    """
+
+    :return:
+    """
     try:
         consumer_key = os.environ['CONSUMER_KEY']
         consumer_secret = os.environ['CONSUMER_SECRET']
         access_token = os.environ['ACCESS_TOKEN']
         access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+
     except KeyError:
-        print("Error, any key not found...")
+        print("Error, any access key not found...")
         print("Error: " + str(sys.exc_info()))
+
+    return tweepy.API(auth)
 
 
 def main():
-
-    #version_string = f"%(prog)s {__version__}\n" + f"{requests.__description__}:  {requests.__version__}\n" + \
-    #                 f"Python:  {platform.python_version()}"
-
     args = parse_args()
     banner()
-    config()
+    api = config_twitter_api()
 
 
 if __name__ == '__main__':
