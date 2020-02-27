7d29676af2e50438f0d570ca144f16023d726d91
Javier Guzman Porras <javier.guzman.porras@gmail.com>
2020-02-27 17:21:36 +0100

    Add documentation for all method and add try...except for authentication process.

--- seada/seada.py
+++ seada/seada.py
@@ -12,8 +12,7 @@ __version__ = 0.1
 
 def banner():
     """
-    Function
-    :return:
+    Function for print a banner
     """
     try:
         banner_file = open("banner2.txt", 'r')
@@ -29,8 +28,8 @@ def banner():
 
 def parse_args():
     """
-
-    :return:
+    Method for get the arguments input of seada program.
+    :return: A Namespace class of argparse.
     """
     parser = argparse.ArgumentParser(prog='seada.py',
                                      description='Sistema de Extracción y Análisis de Datos de fuentes Abiertas',
@@ -45,23 +44,28 @@ def parse_args():
 
 def config_twitter_api():
     """
-
-    :return:
+    Method for get access keys of twitter from environment variables, create the authentication object and
+    create the instance of API class.
+    :return: Instance of "API" class of tweepy
     """
     try:
         consumer_key = os.environ['CONSUMER_KEY']
         consumer_secret = os.environ['CONSUMER_SECRET']
         access_token = os.environ['ACCESS_TOKEN']
         access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
-
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
-
     except KeyError:
         print("Error, any access key not found...")
         print("Error: " + str(sys.exc_info()))
 
-    return tweepy.API(auth)
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    except tweepy.TweepError:
+        print("Fatal error with authentication process")
+        raise
+
+    return api
 
 
 def main():
