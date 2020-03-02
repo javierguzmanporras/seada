3b107a4b8cab93d19734c4beb8aedc7ec58f91bc
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-03-02 22:09:24 +0100

    Add get_json_output static method

--- seada/Utils.py
+++ seada/Utils.py
@@ -4,7 +4,7 @@ import json
 class Utils:
 
     @classmethod
-    def json_to_string(json_information):
+    def json_to_string(cls, json_information):
         """
         Convert object json to string.
         :param json_information: The object that convert to string
@@ -17,3 +17,16 @@ class Utils:
             print("[json_to_string] Error " + str(e))
 
         return None
+
+    @classmethod
+    def get_json_output(cls, file_name, dataset_directory, item):
+        """
+        Generate a json file from user information. If the file exists, append new item.
+        :return: a new json file, or a new item
+        """
+        # file_name = 'user_file.json'
+        path_file = dataset_directory + '/' + file_name
+        #with open(path_file, mode='a+', encoding='utf8') as user_file:
+        with open(path_file, mode='a+') as user_file:
+            json.dump(item, user_file, indent=4) # indent=4 nos formatea la salida del texto.
+
