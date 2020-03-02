8557a3e05d5e0363b82f65a9cbefa9954f73a790
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-03-02 22:08:17 +0100

    New code for mine_tweets and add "add_tweets_to_database" method.

--- seada/TweetMiner.py
+++ seada/TweetMiner.py
@@ -2,37 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import json
-from Tweet import *
-
 import csv
+import tweepy
+import Utils
+from Tweet import *
+from Database import *
 
 
-class TweetMiner():
+class TweetMiner:
 
     def __init__(self):
+        self.tweets_instances = []
         self.tweets = []
 
-    def get_json_output(self):
-        with open('tuit.json', mode='w', encoding='utf8') as tuit_file:
-            json.dump(self.tweets, tuit_file, indent=4) #indent=4 nos formatea la salida del texto.
+    def get_json_output(self, file_name, dataset_directory):
+        print('[TweetMiner][get_json_otput] #tweets: ' + str(len(self.tweets)))
+        Utils.get_json_output(file_name, dataset_directory, self.tweets)
 
-    def toString(self, jsonInformation):
-        """convert json to string"""
-        json_str = json.dumps(jsonInformation._json)
-        #print(type(json_str))
-        #print(json_str)
-
-    def mine_tweet(self, api, username):
-        timeline = api.user_timeline(screen_name=username, include_rts=True, count=500, page=1, tweet_mode="extended")
-        print('#tweets: ' + str(len(timeline)))
-        for item in timeline:
-            #print(type(item))
-            self.toString(item)
+    def mine_tweets(self, api, screen_name, num_tweets):
+        for tweet in tweepy.Cursor(api.user_timeline, screen_name=screen_name, tweet_mode="extended").items(num_tweets):
             t = Tweet()
-            t.set_tuit_information(item)
+            t.set_tweet_information(tweet)
             self.tweets.append(t.tweet)
+            self.tweets_instances.append(t)
 
     def get_report(self):
         Tweet.get_report()
+
+    def add_tweets_to_database(self, db, connection):
+        for tweet in self.tweets_instances:
+            db.create_tweet(connection, tweet.get_tuple_output())
+
 
 
