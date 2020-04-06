778591683dbf7ab284e5036e28d0b402d60df87f
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-04-06 12:15:23 +0200

    changed with works with seada main calls

--- seada/TweetStreaming.py
+++ seada/TweetStreaming.py
@@ -1,14 +1,16 @@
-import tweepy
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from MyStreamListener import *
 
 
-class TweetStreaming():
+class TweetStreaming:
 
     def __init__(self):
         pass
 
-    def start(self, api):
-        myStreamListener = MyStreamListener()
-        myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-        myStream.filter(track=['#paro'])        #filter to stream all tweets containing the word python
-        #myStream.filter(follow=["2211149702"])  #filter to stream tweets by a specific user
+    def start(self, api, track_list, args, db, connection, dataset_directory):
+        my_stream_listener = MyStreamListener(args, db, connection, dataset_directory)
+        my_stream = tweepy.Stream(auth=api.auth, listener=my_stream_listener)
+        my_stream.filter(track=track_list)  # filter to stream all tweets containing the word python
+        # myStream.filter(follow=["2211149702"])  #filter to stream tweets by a specific user
