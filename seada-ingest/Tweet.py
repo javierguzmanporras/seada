20d05c4e68c4eb4b30784a365c7f49cb0a974934
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-04-15 07:55:00 +0200

    Clean code, add method doc and create/delete some attributes

--- seada-ingest/Tweet.py
+++ seada-ingest/Tweet.py
@@ -6,14 +6,8 @@ from SeadaUtils import *
 
 class Tweet:
     """Tweets of Users"""
-    languages = {}
-    sources = {}
-    places = {}
-    hashtags = {}
-    ntweets = 0
 
     def __init__(self):
-        # tweet information (19)
         self.created_at = ""
         self.id = ""
         self.text = ""
@@ -22,7 +16,6 @@ class Tweet:
         self.in_reply_to_status_id_str = ""
         self.in_reply_to_user_id_str = ""
         self.in_reply_to_screen_name = ""
-        #user
         self.coordinates = ""
         self.place = ""
         self.contributors = ""
@@ -33,7 +26,10 @@ class Tweet:
         self.retweeted = ""
         self.possibly_sensitive = ""
         self.lang = ""
-        #self.user_name = ""
+        self.user_id = ""
+        self.user_name = ""
+        self.user = ""
+        self.user_screen_name = ""
         self.entities_hashtags = []
         self.entities_user_mentions = []
         self.entities_urls = []
@@ -52,14 +48,6 @@ class Tweet:
         self.tweet['created_at'] = str(item.created_at)
         self.id = item.id
         self.tweet['id'] = item.id
-
-        try:
-            self.text = item.full_text
-            self.tweet['text'] = item.full_text
-        except AttributeError:
-            self.text = item.text
-            self.tweet['text'] = item.text
-
         self.truncated = item.truncated
         self.tweet['truncated'] = item.truncated
         self.source = item.source
@@ -86,10 +74,28 @@ class Tweet:
         self.tweet['favorited'] = item.favorited
         self.retweeted = item.retweeted
         self.tweet['retweet'] = item.retweeted
-        #self.possibly_sensitive = item.possibly_sensitive
-        #self.tuit['possibly_sensitive'] = item.possibly_sensitive
         self.lang = item.lang
         self.tweet['lang'] = item.lang
+        self.user_id = item.user.id
+        self.tweet['user_id'] = item.user.id
+        self.user_name = item.user.name
+        self.tweet['user_name'] = item.user.name
+        self.user_screen_name = item.user.screen_name
+        self.tweet['user_screen_name'] = item.user.screen_name
+        self.user = item.user
+
+        try:
+            self.text = item.full_text
+            self.tweet['text'] = item.full_text
+        except AttributeError:
+            self.text = item.text
+            self.tweet['text'] = item.text
+
+        try:
+            self.possibly_sensitive = item.possibly_sensitive
+            self.tweet['possibly_sensitive'] = item.possibly_sensitive
+        except:
+            self.tweet['possibly_sensitive'] = None
 
         e = item.entities
         if len(e['hashtags']) > 0:
@@ -108,51 +114,22 @@ class Tweet:
         self.tweet['user_mentions'] = self.entities_user_mentions
         self.tweet['urls'] = self.entities_urls
 
-        #get lang
-        if item.lang in self.languages:
-            self.languages[item.lang] += 1
-        else:
-            self.languages[item.lang] = 1
-
-        #get source
-        if item.source in self.sources:
-            self.sources[item.source] += 1
-        else:
-            self.sources[item.source] = 1
-
-        #get places
-        if item.place:
-            if item.place.name in self.places:
-                self.places[item.place.name] += 1
-            else:
-                self.places[item.place.name] = 1
-
-        for ht in self.entities_hashtags:
-            if ht in self.hashtags:
-                self.hashtags[ht] += 1
-            else:
-                self.hashtags[ht] = 1
-
-
-
-    @classmethod
-    def get_report(self):
-        for key, val in self.languages.items():
-            print(str(key) + " => " + str(val))
-
-        for key, val in self.sources.items():
-            print(str(key) + " => " + str(val))
-
-        for key, val in self.places.items():
-            print(str(key) + " => " + str(val))
-
-        for key, val in self.hashtags.items():
-            print(str(key) + " => " + str(val))
-
     def get_json_output(self, file_name, dataset_directory):
+        """
+        Generate a json output file for this instance.
+        :param file_name: the name of the json file.
+        :param dataset_directory: where the file will create.
+        :return: a json file in dataset_directory.
+        """
         SeadaUtils.get_json_output(file_name, dataset_directory, self.tweet)
 
     def get_csv_output(self, file_name, dataset_directoy):
+        """
+        Generate a csv output file for this instance.
+        :param file_name: the name of the csv file.
+        :param dataset_directoy: where the file will create
+        :return: a csv file in dataset_directory.
+        """
         item = list(self.get_tuple_output())
         item.pop(-1)
         SeadaUtils.get_csv_output(file_name, dataset_directoy, item)
@@ -186,8 +163,3 @@ class Tweet:
                        self.is_quote_status, self.retweet_count, self.favorite_count, self.favorited, self.retweeted,
                        self.possibly_sensitive, self.lang, self.raw_tweet)
         return tuple_tweet
-
-
-
-    # user
-    # self.user_name = ""
