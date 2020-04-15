afb20c3b258d64aa888e15d41abab6ebde6e00b3
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-04-15 07:53:03 +0200

    Clean code and fix open correct mapping file.

--- seada-ingest/ElasticsearchUtils.py
+++ seada-ingest/ElasticsearchUtils.py
@@ -29,18 +29,18 @@ class ElasticSearchUtils:
     def create_index(self, index_name, mapping_file):
         try:
             if not self.es_connection.indices.exists(index_name):
-                print("[TwitterUser.create_index] Index " + index_name + "not found.")
+                print("[TwitterUser.create_index] Index " + index_name + " not found.")
                 print("[TwitterUser.create_index] Creating index in elasticsearch...")
 
                 path = "elastic/" + mapping_file
-                with open('elastic/elasticsearch_twitter_user_index_mapping.json', 'r') as mapping_file:
-                    data = mapping_file.read()
+                with open(path, 'r') as mf:
+                    data = mf.read()
 
                 index_definition = json.loads(data)
 
-                print("[TwitterUser.create_index] " + str(type(index_definition)))
-                print("[TwitterUser.create_index]" + str(index_definition))
-                print()
+                #print("[TwitterUser.create_index] " + str(type(index_definition)))
+                #print("[TwitterUser.create_index]" + str(index_definition))
+                #print()
 
                 response = self.es_connection.indices.create(index=index_name, body=index_definition)
                 print(response)
@@ -50,22 +50,10 @@ class ElasticSearchUtils:
 
 
     def store_information_to_elasticsearch(self, index_name, info):
-        #index_name = "twitter_user"
         #doc_type = "user"
-        #self.user['@timestamp'] = datetime.now()
         info['@timestamp'] = datetime.now()
 
-        # print("id: " + str(type(self.user['id'])))
-        # print("name: " + str(type(self.user['name'])))
-        # print("screen_name: " + str(type(self.user['screen_name'])))
-        # print("followers_count: " + str(type(self.user['followers_count'])))
-        # print("friend_count: " + str(type(self.user['friends_count'])))
-        # print("geo_enabled" + str(type(self.user['geo_enabled'])))
-
-        #self.create_index(index_name=index_name)
-
         try:
-            #response = self.elasticsearch.index(index=index_name, id=9, body=self.user)
             response = self.es_connection.index(index=index_name, id=info['id'], body=info)
             print("[TwitterUser.ingest_user_information_to_elasticsearch_v2]" + str(response))
         except Exception as es:
