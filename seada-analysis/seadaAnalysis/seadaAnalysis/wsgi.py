006e8114ed6a54497aa72f456ca1335dc5a00c82
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-04-20 18:09:23 +0200

    First commit

---
+++ seada-analysis/seadaAnalysis/seadaAnalysis/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for seadaAnalysis project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seadaAnalysis.settings')
+
+application = get_wsgi_application()
