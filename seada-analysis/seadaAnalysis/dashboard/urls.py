6c9df6c798ee9755dc508a582ea6e8e60b3aa0da
Javier Guzman Porras <javier.guzman.porras@gmail.com>
2020-04-21 19:46:34 +0200

    Add html base and first jscript test

--- seada-analysis/seadaAnalysis/dashboard/urls.py
+++ seada-analysis/seadaAnalysis/dashboard/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.dashboard, name='dashboard'),
+    path('chart/', views.get_chart_data, name='chart_data'),
 ]
--- seada-analysis/seadaAnalysis/dashboard/views.py
+++ seada-analysis/seadaAnalysis/dashboard/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import JsonResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def dashboard(request):
+    template = 'dashboard/dashboard.html'
+    context = {
+        'title': 'Django chart.js'
+    }
+    return render(request, template, context)
+
+
+def get_chart_data(request):
+    """
+    only responsibility to aggregate the data the return a JSON response with the labels and data
+    :param request:
+    :return:
+    """
+    labels = ['A', 'B', 'C']
+    data = ['10', '10', '10']
+
+    return JsonResponse(data={
+        'labels': labels,
+        'data': data,
+    })
+
