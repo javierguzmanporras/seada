cc57b6fd52833c4d5fe546c750ecc8030db934f5
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-05-12 06:57:46 +0200

    Join MyStreamListener class along with TweetStreaming class in only one file.

--- seada-ingest/tweetStreaming.py
+++ seada-ingest/tweetStreaming.py
@@ -1,6 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import tweepy
 
-from Tweet import *
+from twitterTweet import Tweet
+
+
+class TweetStreaming:
+
+    def __init__(self):
+        pass
+
+    def start(self, api, track_list, args, db, connection, dataset_directory):
+        my_stream_listener = MyStreamListener(args, db, connection, dataset_directory)
+        my_stream = tweepy.Stream(auth=api.auth, listener=my_stream_listener)
+        my_stream.filter(track=track_list)  # filter to stream all tweets containing the word python
+        # myStream.filter(follow=["2211149702"])  #filter to stream tweets by a specific user
 
 
 class MyStreamListener(tweepy.StreamListener):
