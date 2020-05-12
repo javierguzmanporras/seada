d1f600f960635bf6322c4fee48cebd383f0689a6
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-05-12 07:34:53 +0200

    Add new django app "twitter"

--- seada-analysis/seadaAnalysis/seadaAnalysis/settings.py
+++ seada-analysis/seadaAnalysis/seadaAnalysis/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'twitter.apps.TwitterConfig',
     'dashboard.apps.DashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
