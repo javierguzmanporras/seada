8524e9f817db79b49eb6903f4c2724618f125a8c
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-05-12 07:35:11 +0200

    New include urls.py

--- seada-analysis/seadaAnalysis/seadaAnalysis/urls.py
+++ seada-analysis/seadaAnalysis/seadaAnalysis/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
+    path('twitter/', include('twitter.urls')),
     path('admin/', admin.site.urls),
 ]
