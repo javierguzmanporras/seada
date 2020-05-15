c05e819011a42cd27bfb7ff667267d48af983ba6
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-05-15 08:25:57 +0200

    Add index home site with Templateview.as_view

--- seada-analysis/seadaAnalysis/seadaAnalysis/urls.py
+++ seada-analysis/seadaAnalysis/seadaAnalysis/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('dashboard/', include('dashboard.urls')),
     path('twitter/', include('twitter.urls')),
     path('admin/', admin.site.urls),
