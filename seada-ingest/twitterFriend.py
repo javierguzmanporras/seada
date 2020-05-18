38e737572c2a58157eaced925e338043f0dd7c15
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-05-18 20:45:40 +0200

    Add output code.

--- seada-ingest/twitterFriend.py
+++ seada-ingest/twitterFriend.py
@@ -4,6 +4,7 @@
 import sys
 import tweepy
 import logging
+import seadaUtils
 
 from timer import Timer
 from tqdm import tqdm
@@ -12,14 +13,13 @@ from tqdm import tqdm
 class TwitterFriends:
     """Information about a Twitter user's friends"""
 
-    def __init__(self, username, api):
+    def __init__(self, api):
         self.api = api
-        self.username = username
         self.friends_id_list = []        # List with user id of user's friends
         self.friends_user_list = []      # List with instances of twitterUser class
 
-    def get_user_friends(self):
-        user = self.api.get_user(self.username)
+    def get_user_friends(self, username):
+        user = self.api.get_user(username)
         total = user.friends_count
 
         for friend_id in tqdm(tweepy.Cursor(self.api.friends_ids, screen_name=user).items(total), total=total,
@@ -35,6 +35,25 @@ class TwitterFriends:
     def get_output(self):
         for friend in self.friends_user_list:
             print(friend.name)
+
+    def get_json_output(self, file_name, dataset_directory):
+        """
+        Generate a json file from user's friends information. If the file exists, append new item
+        :return: a new json file, or a new item
+        """
+        item = {'data': self.friends_id_list}
+        seadaUtils.SeadaUtils.get_json_output(file_name=file_name, dataset_directory=dataset_directory, item=item)
+
+    def get_cvs_output(self, file_name, dataset_directory):
+        """
+        Generate a CVS file from user information instance. If the file exists, append info in new row.
+        :return: a new csv file or a new row.
+        """
+        pass
+
+    def get_tuple_output(self):
+        pass
+
 
 
 
