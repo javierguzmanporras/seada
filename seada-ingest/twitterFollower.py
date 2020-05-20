929b657259f7447ca444ece755357a653a7c7acd
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-05-20 19:13:53 +0200

    code changed: json output

--- seada-ingest/twitterFollower.py
+++ seada-ingest/twitterFollower.py
@@ -1,50 +1,28 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import tweepy
-import logging
-
-from tqdm import tqdm
-from twitterUser import TwitterUser
 from outputInterface import OutputInterface
+from outputUtilities import OutputUtilities
 
 
 class TwitterFollowers(OutputInterface):
     """Information about a Twitter user's followers"""
 
-    def __init__(self, username, api):
-        self.username = username
-        self.api = api
-        self.followers_id_list = []         # List with id of user's followers
-        self.followers_user_list = []       # List with instances of twitterUser class
-
-    def get_user_followers(self):
-
-        user = self.api.get_user(self.username)
-        total = user.followers_count
-
-        for follower_id in tqdm(tweepy.Cursor(self.api.followers_ids, screen_name=self.username).items(total),
-                                total=total, desc="[+] Get Followers"):
-            try:
-                self.followers_id_list.append(follower_id)
-                api_follower_user = self.api.get_user(follower_id)
-                tu = TwitterUser("")
-                tu.set_user_information(api_follower_user)
-                self.followers_user_list.append(tu)
-            except Exception as exception:
-                print("[+] Error: {}. More info: {}".format(exception, sys.exc_info()))
-                logging.critical("[TwitterFollowers.get_user_followers] Error {}".format(sys.exc_info()))
-
-    def get_output(self):
-        for follower in self.followers_user_list:
-            print(follower.name)
+    def __init__(self, user_id, follower_id):
+        self.user_id = user_id
+        self.follower_id = follower_id
 
     def get_csv_output(self, file_name: str, dataset_directory: str):
         pass
 
     def get_json_output(self, file_name: str, dataset_directory: str):
-        pass
+        """
+                Call to outputUtilities class for generate a json file from user's friends information.
+                If the file exists, append new item
+                :return:
+                """
+        item = {'follower_id': self.follower_id, 'user_id': self.user_id}
+        OutputUtilities.get_json_output(file_name=file_name, dataset_directory=dataset_directory,
+                                        item=item, datatag='followers_list')
 
     def get_tuple_output(self) -> tuple:
         pass
