2953487603b50a542cfe99ee7e0dd887fc981114
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-05-22 11:22:35 +0200

    Developed get_csv_output and get_tuple_output methods.

--- seada-ingest/twitterFavorites.py
+++ seada-ingest/twitterFavorites.py
@@ -3,6 +3,8 @@
 from outputInterface import OutputInterface
 from outputUtilities import OutputUtilities
 
+import json
+
 
 class TwitterFavorites(OutputInterface):
     """Information about a Twitter user's Favorites"""
@@ -12,7 +14,8 @@ class TwitterFavorites(OutputInterface):
         self.favorite = favorite
 
     def get_csv_output(self, file_name: str, dataset_directory: str):
-        pass
+        item = self.get_tuple_output()
+        OutputUtilities.get_csv_output(file_name=file_name, dataset_directory=dataset_directory, item=item)
 
     def get_json_output(self, file_name: str, dataset_directory: str):
         """
@@ -34,9 +37,20 @@ class TwitterFavorites(OutputInterface):
             'favorite_user_mentions': self.favorite.entities_user_mentions,
             'favorite_urls': self.favorite.entities_urls
         }
-
         OutputUtilities.get_json_output(file_name=file_name, dataset_directory=dataset_directory,
                                         item=item, datatag='favorites_list')
 
     def get_tuple_output(self) -> tuple:
-        pass
+        text = self.favorite.text.replace("\n", "")
+
+        # You could pickle/marshal/json your array and store it as binary/varchar/json field in your database.
+        hashtags_json = json.dumps(self.favorite.entities_hashtags)
+        user_mentions_json = json.dumps(self.favorite.entities_user_mentions)
+        urls_json = json.dumps(self.favorite.entities_urls)
+
+        tuple_favorites = (self.favorite.id, self.user_id, str(self.favorite.created_at), text,
+                           self.favorite.source, str(self.favorite.place),
+                           self.favorite.retweet_count, self.favorite.favorite_count, self.favorite.lang,
+                           hashtags_json, user_mentions_json, urls_json)
+
+        return tuple_favorites
