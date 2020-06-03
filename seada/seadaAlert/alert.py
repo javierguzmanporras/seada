a4acd9d811a5ee647b8bd863d8876094eb41bace
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-06-03 19:20:03 +0200

    add new vars and try/except operator input.

--- seada/seadaAlert/alert.py
+++ seada/seadaAlert/alert.py
@@ -1,17 +1,21 @@
 class Alert:
 
     def __init__(self, alert_info):
-
-        # print(type(alert_info['name']))
-        # print(alert_info['name'])
-
         self.name = alert_info['name']
         self.type = alert_info['type']
         self.index = alert_info['index']
-        self.list = alert_info['list']
+        self.terms_list = alert_info['list']
+        self.interval_time = alert_info['sleep_interval']
         self.outputs = alert_info['outputs']
         self.emails = alert_info['emails']
+        self.email = alert_info['email']
         self.telegram = alert_info['telegram']
+
+        try:
+            self.operator = alert_info['operator']
+        except KeyError:
+            self.operator = 'or'
+
         self.alert_info = alert_info
 
     def __str__(self):
