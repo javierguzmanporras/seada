0c480c33a803da683a3b5537f510f6e1bf84cf42
Javier Guzmán Porras <javier.guzman.porras@gmail.com>
2020-06-03 19:16:48 +0200

    Add vars for alert and stream tools. add return information in store_information_to_elasticsearch

--- seada/seadaIngest/elasticsearchHandler.py
+++ seada/seadaIngest/elasticsearchHandler.py
@@ -10,6 +10,7 @@ from elasticsearch import Elasticsearch
 
 class ElasticSearchUtils:
 
+    # seadaIngest's vars
     twitter_user_index_name = "twitter_user"
     twitter_user_mapping_file = "elasticsearch_twitter_user_index_mapping.json"
     twitter_tweet_index_name = "twitter_tweets"
@@ -20,6 +21,14 @@ class ElasticSearchUtils:
     twitter_follower_mapping_file = "elasticsearch_twitter_follower_index_mapping.json"
     twitter_favorite_index_name = "twitter_favorite"
     twitter_favorite_mapping_file = "elasticsearch_twitter_favorite_index_mapping.json"
+
+    # streaming's vars
+    twitter_streaming_tweet_index_name = 'twitter_streaming_tweets'
+    twitter_streaming_tweet_mapping_file = "elasticsearch_twitter_tweets_index_mapping.json"
+
+    # seadaAlert's vars
+    twitter_alert_index_name = 'twitter_alert'
+    twitter_alert_mapping_file = 'elasticsearch_twitter_alert_index_mapping.json'
 
     def __init__(self, es_host, es_port):
         self.es_host = es_host
@@ -37,7 +46,7 @@ class ElasticSearchUtils:
             logging.critical("[ElasticsearchUtils.connect] - Fail to connected to elasticsearch...")
         return _es
 
-    def create_index(self, index_name, mapping_file, debug):
+    def create_index(self, index_name, mapping_file, debug, path='seadaIngest/elastic/'):
         try:
             if not self.es_connection.indices.exists(index_name):
                 print('[+] Index {} not found.'.format(index_name))
@@ -45,7 +54,7 @@ class ElasticSearchUtils:
                 logging.warning("[elasticsearchUtils.create_index] Index " + index_name + " not found.")
                 logging.info("[elasticsearchUtils.create_index] Creating index in elasticsearch...")
 
-                path = "elastic/" + mapping_file
+                path = path + mapping_file
                 with open(path, 'r') as mf:
                     data = mf.read()
 
@@ -61,15 +70,19 @@ class ElasticSearchUtils:
             logging.critical('[elasticsearchUtils.create_index] Error in indexing data')
 
     def store_information_to_elasticsearch(self, index_name, info, debug):
+        result = None
         document = dict(info)
         document['@timestamp'] = datetime.now()
 
         try:
             response = self.es_connection.index(index=index_name, id=document['id'], body=document)
-
+            result = response['result']
             if debug:
                 print('[+] {}'.format(response))
+
             logging.info("[elasticsearchUtils.ingest_user_information_to_elasticsearch]" + str(response))
         except Exception as es:
-            print('[+] Exception: {}'.format(es))
+            print('[+] Exception at store info in database: {}'.format(es))
             logging.critical("[elasticsearchUtils.ingest_user_information_to_elasticsearch]" + str(es))
+
+        return result
